01f579c0bcfdf8296db5c64df361caf52286f196
pyloris <wanishoaib@protonmail.com>
2023-09-15 21:24:55 +0530

    added model support

---
+++ src/Model/Model.php
@@ -0,0 +1,28 @@
+<?php
+namespace sirJuni\Framework\Model;
+
+use sirJuni\Framework\Helper\HelperFuncs;
+
+class Database {
+    private $db;
+    public function __construct() {
+        dbConnect();
+    }
+
+    private function dbConnect() {
+        $dsn = DB_TYPE . ":host=" . DB_HOST . ";dbname=" . DB_NAME . ";";
+        $dbUser = DB_USER;
+        $dbPass = DB_PASS;
+
+        try {
+            $this->db = new PDO($dsn, $dbUser, $dbPass);
+            $this->db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $this->db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+        }
+        catch (PDOException $e) {
+            HelperFuncs::report($e);
+        }
+    }
+}
+
+?>
