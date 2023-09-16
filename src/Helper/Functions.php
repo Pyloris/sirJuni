63db946336bcc2768643dc33452bbdafb574c58e
pyloris <wanishoaib@protonmail.com>
2023-09-16 22:08:34 +0530

    fixed helper functions

--- src/Helper/Functions.php
+++ src/Helper/Functions.php
@@ -1,11 +1,11 @@
 <?php
 namespace sirJuni\Framework\Helper;
 
-public static function report($e) {
+function report($e) {
     echo("File : " . $e->getFile() . "<br\> Line : " . $e->getLine() . "<br\> Message : " . $e->getMessage());
 }
 
-public static function redirect($url) {
+function redirect($url) {
     header("Location: $url");
 }
 
