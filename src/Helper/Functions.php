56abb7795fbe77e25ad5a9b831f99f0fc32f936b
pyloris <wanishoaib@protonmail.com>
2023-09-17 14:11:08 +0530

    wrapped functions in class

--- src/Helper/Functions.php
+++ src/Helper/Functions.php
@@ -1,12 +1,14 @@
 <?php
 namespace sirJuni\Framework\Helper;
 
-function report($e) {
-    echo("File : " . $e->getFile() . "<br\> Line : " . $e->getLine() . "<br\> Message : " . $e->getMessage());
-}
+class Helper {
+    function public static report($e) {
+        echo("File : " . $e->getFile() . "<br\> Line : " . $e->getLine() . "<br\> Message : " . $e->getMessage());
+    }
 
-function redirect($url) {
-    header("Location: $url");
+    function public static redirect($url) {
+        header("Location: $url");
+    }
 }
 
 ?>
