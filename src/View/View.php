fe7d9269e0f8c32073419060176e93dcf40c0353
pyloris <wanishoaib@protonmail.com>
2023-09-17 11:41:25 +0530

    fixed the end slash for template dir

--- src/View/View.php
+++ src/View/View.php
@@ -5,13 +5,13 @@ namespace sirJuni\Framework\View;
 class VIEW {
     public static $path;
     public static function set_path($path) {
-        self::$path = $path;
+        self::$path = rtrim($path, '\\');
     }
 
     public static function init($page, $data=NULL) {
         if ($data != NULL)
             extract($data);
-        include self::$path . "$page";
+        include self::$path . "\\$page";
     }
 }
 
