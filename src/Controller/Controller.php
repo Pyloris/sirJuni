cb0a57c3153dbea2aadbbf750ff1ce89120ff725
pyloris <wanishoaib@protonmail.com>
2023-09-17 20:39:54 +0530

    added methods to the controller

--- src/Controller/Controller.php
+++ src/Controller/Controller.php
@@ -3,8 +3,11 @@
 namespace sirJuni\Framework\Controller;
 
 abstract class Controller {
-    abstract public function index($request, $data);
-    abstract public function update($request, $data);
+    abstract public function index($request);
+    abstract public function show($request);
+    abstract public function create($request);
+    abstract public function store($request);
+    abstract public function delete($request);
 }
 
 ?>
