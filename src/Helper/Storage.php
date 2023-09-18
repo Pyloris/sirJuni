01a7278abe1660e9fd870896389e972b23892d1c
pyloris <wanishoaib@protonmail.com>
2023-09-18 22:35:37 +0530

    added key storage inside of the session so that data accessed by Storage is only the data that was added to it

--- src/Helper/Storage.php
+++ src/Helper/Storage.php
@@ -1,60 +1,71 @@
 <?php
 namespace sirJuni\Framework\Helper;
 
-
 class Storage {
 
-    public static $keys = [];
+    // initiate key storage
+    public static function init() {
+        if (session_status() == PHP_SESSION_NONE) {
+            session_start();
+        }
+        $_SESSION['key_store'] = [];
+        session_write_close();
+    }
 
     public static function addData($key, $value) {
-        if (session_status() == PHP_SESSION_NONE)
+        if (session_status() == PHP_SESSION_NONE) {
             session_start();
+        }
         $_SESSION[$key] = $value;
+        $_SESSION['key_store'][] = $key;
         session_write_close();
-        array_push(self::$keys, $key);
     }
 
     public static function getData($key) {
-        if (session_status() == PHP_SESSION_NONE)
+        if (session_status() == PHP_SESSION_NONE) {
             session_start();
-        if (isset($_SESSION[$key]) and boolval(array_search($key, self::$keys)+1)){
+        }
+        if (isset($_SESSION[$key]) && in_array($key, $_SESSION['key_store'])) { // Use in_array to check if key exists in the array
             session_write_close();
             return $_SESSION[$key];
-        }
-        else{
+        } else {
             session_write_close();
             return NULL;
         }
     }
 
     public static function dump() {
-        if (session_status() == PHP_SESSION_NONE)
+        if (session_status() == PHP_SESSION_NONE) {
             session_start();
+        }
         $data = [];
-        foreach(self::$keys as $key)
+        foreach ($_SESSION['key_store'] as $key) {
             $data[$key] = $_SESSION[$key];
+        }
         session_write_close();
-    
+
         return $data;
     }
 
     public static function addBucket($arr) {
-        if (session_status() == PHP_SESSION_NONE)
+        if (session_status() == PHP_SESSION_NONE) {
             session_start();
-        foreach($arr as $key=>$value) {
-            array_push(self::$keys, $key);
+        }
+        foreach ($arr as $key => $value) {
+            $_SESSION['key_store'][] = $key;
             $_SESSION[$key] = $value;
         }
         session_write_close();
     }
 
     public static function release() {
-        if (session_status() == PHP_SESSION_NONE)
+        if (session_status() == PHP_SESSION_NONE) {
             session_start();
-        foreach(self::$keys as $key) {
+        }
+        foreach ($_SESSION['key_store'] as $key) {
             unset($_SESSION[$key]);
         }
-        self::keys = [];
+        $_SESSION['key_store'] = [];
         session_write_close();
     }
 }
