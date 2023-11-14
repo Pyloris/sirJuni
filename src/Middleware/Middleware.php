1087f5f719bad546cb5310015ae6b1ee0c1cf49e
pyloris <wanishoaib@protonmail.com>
2023-11-14 18:53:22 +0530

    implemented some middleware methods

--- src/Middleware/Middleware.php
+++ src/Middleware/Middleware.php
@@ -1,11 +1,21 @@
 <?php
 namespace sirJuni\Framework\Middleware;
 
+use sirJuni\Framework\Helper\HelperFuncs;
+
 // Middleware should implement following methods
-abstract class Middleware {
-    abstract public static function set_fallback_route($route);
-    abstract public static function handle($request);
-    abstract public static function fallback();
+class Middleware {
+    public static $url;
+
+    public static function set_fallback_route($route) {
+        self::$url = $route;
+    }
+
+    public static function handle($request);
+
+    abstract public static function fallback() {
+        HelperFuncs::redirect(self::$url);
+    }
 }
 
 
