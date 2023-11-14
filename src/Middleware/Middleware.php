fb449297cec4049ee595f352bcff04824c071dfe
pyloris <wanishoaib@protonmail.com>
2023-11-14 19:06:51 +0530

    fixed more bugs

--- src/Middleware/Middleware.php
+++ src/Middleware/Middleware.php
@@ -11,7 +11,7 @@ class Middleware {
         self::$url = $route;
     }
 
-    abstract public static function fallback() {
+    public static function fallback() {
         HelperFuncs::redirect(self::$url);
     }
 }
