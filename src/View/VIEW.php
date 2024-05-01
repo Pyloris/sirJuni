5322f666578c0da6246977ed4616cf8724300507
Shoaib Wani <wanishoaib@protonmail.com>
2024-05-01 17:29:02 +0530

    added VIEW

---
+++ src/View/VIEW.php
@@ -0,0 +1,44 @@
+<?php
+namespace sirJuni\Framework\View;
+
+
+class VIEW {
+    public static $path;
+    public static $request;
+
+    public static function set_path($path) {
+        // trim \
+        self::$path = rtrim($path, '\\');
+        // trim /
+        self::$path = rtrim($path, '/');
+    }
+
+    // set the request context from Application
+    public static function set_context($request) {
+        self::$request = $request;
+    }
+
+    public static function init($page, $data=NULL) {
+
+        // generate the route placeholder context
+        $context = $data ?? [];
+        
+        foreach(self::$request->getRouteKeys() as $key) {
+            $context[$key] = self::$request->getRouteValue($key);
+        }
+
+        // unpack get variables too
+        foreach(self::$request->queryKeys() as $key) {
+            $context[$key] = self::$request->queryData($key);
+        }
+
+        // provides all the route variables and data passed
+        // as variables.
+        if ($context)
+            extract($context);
+
+        include self::$path . "/$page";
+    }
+}
+
+?>
